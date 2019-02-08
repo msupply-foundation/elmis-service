dfbb92864e765adae3f6e14c3d7ceb9dc0004a07
joshxg <griffinjoshua5@gmail.com>
2019-02-08 14:41:56 +1300

    Add programValidation tests

---
+++ src/tests/validation/programValidation.test.js
@@ -0,0 +1,44 @@
+import programValidation from '../../validation';
+import errorObject from '../../errors/errors';
+
+test('should return id when code matches', () => {
+  const testingObject = [
+    { id: 1, code: 'a' },
+    { id: 2, code: 'b' },
+    { id: 3, code: 'c' },
+    { id: 10, code: 'd' },
+  ];
+  const testingCode = 'd';
+  expect(programValidation(testingCode, testingObject));
+});
+
+test('should throw on unmatched code', () => {
+  const testingObject = [
+    { id: 1, code: 'a' },
+    { id: 2, code: 'b' },
+    { id: 3, code: 'c' },
+    { id: 10, code: 'd' },
+  ];
+  const testingCode = 'e';
+
+  let errorCatcher;
+  try {
+    programValidation(testingCode, testingObject);
+  } catch (e) {
+    errorCatcher = e;
+  }
+  expect(errorCatcher).toEqual(errorObject('Could not find the Program'));
+});
+
+test('should throw on empty array', () => {
+  const testingObject = [];
+  const testingCode = 'a';
+
+  let errorCatcher;
+  try {
+    programValidation(testingCode, testingObject);
+  } catch (e) {
+    errorCatcher = e;
+  }
+  expect(errorCatcher).toEqual(errorObject('Could not find the Program'));
+});
