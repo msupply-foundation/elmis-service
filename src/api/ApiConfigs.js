52eebf2b9bad563bf1f72980de554b06e1e25f7e
joshxg <griffinjoshua5@gmail.com>
2019-02-10 12:24:25 +1300

    Add change incorrect login url

--- src/api/ApiConfigs.js
+++ src/api/ApiConfigs.js
@@ -9,7 +9,7 @@ export default class ApiConfigs {
   static cookie = '';
 
   static BASE_CONFIG = {
-    baseURL: 'http://127.0.0.1:9921',
+    baseURL: 'https://83.96.240.209',
     httpsAgent: new https.Agent({
       rejectUnauthorized: false,
     }),
@@ -21,7 +21,7 @@ export default class ApiConfigs {
 
   static getLoginConfig = (username, password) => ({
     ...ApiConfigs.BASE_CONFIG,
-    url: '/cookie',
+    url: '/j_spring_security_check',
     withCredentials: true,
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
     data: qs.stringify({ j_username: username, j_password: password }),
