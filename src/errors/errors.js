9ce0943302bb6d7183102f844f6c08fabbebc2f0
joshxg <griffinjoshua5@gmail.com>
2019-02-10 15:29:08 +1300

    Add AUTH error const

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -3,6 +3,7 @@ export const ERROR_SERVER = method => `${method} Error: Unknown Server Error`;
 export const ERROR_UNKNOWN = method => `${method} Error: Unknown status code error (Not: 401, 500)`;
 export const ERROR_REQUEST = method => `${method} Error: Request malformed`;
 export const ERROR_COOKIE = method => `${method} Error: Unable to set session cookie`;
+export const ERROR_AUTHENTICATION = method => `${method} Error: Authentication error`;
 export const ERROR_VALIDATION = (method, block) => {
   return `${method} Error: Malformed input during ${block} check`;
 };
