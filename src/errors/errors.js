a7b87c50e65ee2d3fe2915f7fa1d5a1e34adbb10
joshxg <griffinjoshua5@gmail.com>
2019-02-17 12:14:08 +1300

    Add ERROR_UNKNOWN_RESPONSE code as refactor of ERROR_UNKOWN

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -11,6 +11,8 @@ export const ERROR_RESPONSE = method => `${method} Error: Malformed response`;
 export const ERROR_LOGIN = method => `${method} Error: Incorrect username or password`;
 export const ERROR_SERVER = method => `${method} Error: Unknown Server Error`;
 export const ERROR_UNKNOWN = method => `${method} Error: Unknown status code error (Not: 401, 500)`;
+export const ERROR_UNKNOWN_RESPONSE = (method, status) =>
+  `${method} Error: Unknown status code error - ${status}`;
 export const ERROR_REQUEST = method => `${method} Error: Request malformed`;
 export const ERROR_COOKIE = method => `${method} Error: Unable to set session cookie`;
 export const ERROR_AUTHENTICATION = method => `${method} Error: Authentication error`;
