80d12529badc22bbc59600d5be84633358ec9cc6
joshxg <griffinjoshua5@gmail.com>
2019-02-17 12:09:12 +1300

    Add ERROR_RESPONSE code

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -7,6 +7,7 @@
  * occured.
  */
 
+export const ERROR_RESPONSE = method => `${method} Error: Malformed response`;
 export const ERROR_LOGIN = method => `${method} Error: Incorrect username or password`;
 export const ERROR_SERVER = method => `${method} Error: Unknown Server Error`;
 export const ERROR_UNKNOWN = method => `${method} Error: Unknown status code error (Not: 401, 500)`;
