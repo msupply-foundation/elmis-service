bf356fac8dfbe28f34c8cf6f379dee84190f525b
joshxg <griffinjoshua5@gmail.com>
2019-02-17 15:31:15 +1300

    Add approveRequisition tests

---
+++ src/tests/requests/approveRequisition.test.js
@@ -0,0 +1,12 @@
+import '@babel/polyfill';
+
+beforeEach(() => {
+  jest.resetModules();
+  jest.dontMock('axios');
+});
+
+test('should return true from response', async () => {
+  jest.doMock('axios', () => jest.fn(() => ({ data: { success: 'R&R approved successfully!' } })));
+  const { approveRequisition } = require('../../requests');
+  expect(await approveRequisition({ cookie: '', baseURL: '', requisitionId: 1 })).toEqual(true);
+});
