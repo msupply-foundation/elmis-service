e7af05c4e982f4315f9c2513e3d454313d324142
joshxg <griffinjoshua5@gmail.com>
2019-02-19 21:14:52 +1300

    Add deleteRequisition tests

---
+++ src/tests/requests/deleteRequisition.test.js
@@ -0,0 +1,12 @@
+import '@babel/polyfill';
+
+beforeEach(() => {
+  jest.resetModules();
+  jest.dontMock('axios');
+});
+
+test('should return true from response', async () => {
+  jest.doMock('axios', () => jest.fn(() => ({ status: 200 })));
+  const { deleteRequisition } = require('../../requests');
+  expect(await deleteRequisition({ cookie: '', baseURL: '', requisitionId: 1 })).toEqual(true);
+});
