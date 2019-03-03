6db7575dcb6782d256ffa5c68e9a9c3138aaf52e
joshxg <griffinjoshua5@gmail.com>
2019-03-03 22:01:13 +1300

    Add remove index leftover code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,2 @@
-import { login } from './requests';
-
 export * from './requests';
 export * from './integrate';
-
-const options = {
-  baseURL: 'https://83.96.240.209/',
-  username: 'jangaman',
-  password: 'kathmanu312',
-};
-
-async function sendRequest() {
-  try {
-    const response = await login(options);
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-sendRequest();
