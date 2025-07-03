ffcaa6faaa2ac9c9730216efaa603726ca911ed0
raviSussol <ravi.shrestha@sussol.net>
2025-07-03 08:16:21 +0545

    201 rename config var

--- src/axiosUtilities.js
+++ src/axiosUtilities.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { getErrorObject } from './errors/errorLookupTable';
 
 /**
- * Creates an http request to an API using axios and wraps it in a safe configuration object.
- * The safe configuration object includes metadata about the request and is used for error handling.
+ * Creates an http request to an API using axios and wraps it in a log configuration object.
+ * The log configuration object includes metadata about the request and is used for error handling.
  * It includes the method, URL, baseURL, params, headers, timeout, and a timestamp.
  * @param {Object} config - The axios request configuration object.
  * @param {String} action - The action being performed, used for error handling.
- * @param {Object} additionalMetadata - Additional metadata to include in the safe config.
- * @returns {Object} - A safe configuration object with response data or an error object.
+ * @param {Object} additionalMetadata - Additional metadata to include in the log config.
+ * @returns {Object} - A log configuration object with response data or an error object.
  */
 async function axiosRequest(config, action, additionalMetadata = {}) {
-  const safeConfig = {
+  const logConfig = {
     method: config.method,
     url: config.url,
     baseURL: config.baseURL,
@@ -24,14 +24,14 @@ async function axiosRequest(config, action, additionalMetadata = {}) {
   try {
     const { status, headers, data } = await axios(config);
     return {
-      request: { ...safeConfig },
+      request: { ...logConfig },
       response: { status, headers, data },
     };
   } catch (error) {
     const { config: _, request, ...otherResponses } = error.response || {};
     throw {
       ...getErrorObject(error, action),
-      request: { ...safeConfig },
+      request: { ...logConfig },
       response: { ...otherResponses },
     };
   }
