fc779edc8ee9db88f76e950e58802d56102122cd
raviSussol <ravi.shrestha@sussol.net>
2025-07-07 14:29:41 +0545

    201 add httpLog in integrate function endpoint

--- src/integrate.js
+++ src/integrate.js
@@ -23,11 +23,39 @@ import requisitionMerge from './requisitionMerge';
 import { errorObject, ERROR_RUNTIME } from './errors/errors';
 
 async function processRequisition(parameterObject) {
-  await updateRequisition(parameterObject);
-  await submitRequisition(parameterObject);
-  await authorizeRequisition(parameterObject);
-  await approveRequisition(parameterObject);
-  await requisitionToOrder(parameterObject);
+  const httpLog = {};
+
+  const updateResult = await updateRequisition(parameterObject);
+  httpLog.updateRequisition = {
+    request: updateResult.request,
+    response: updateResult.response,
+  };
+
+  const submitResult = await submitRequisition(parameterObject);
+  httpLog.submitRequisition = {
+    request: submitResult.request,
+    response: submitResult.response,
+  };
+
+  const authorizeResult = await authorizeRequisition(parameterObject);
+  httpLog.authorizeRequisition = {
+    request: authorizeResult.request,
+    response: authorizeResult.response,
+  };
+
+  const approveResult = await approveRequisition(parameterObject);
+  httpLog.approveRequisition = {
+    request: approveResult.request,
+    response: approveResult.response,
+  };
+
+  const requisitionToOrderResult = await requisitionToOrder(parameterObject);
+  httpLog.requisitionToOrder = {
+    request: requisitionToOrderResult.request,
+    response: requisitionToOrderResult.response,
+  };
+
+  return httpLog;
 }
 
 async function createParameterObject({ options, requisition }) {
@@ -57,38 +85,96 @@ async function createParameterObject({ options, requisition }) {
 export async function integrate(inputParameters) {
   let requisitionHasBeenCreated = false;
   let parameterObject;
+  const httpLog = {
+    createRequisition: null,
+    updateRequisition: null,
+    submitRequisition: null,
+    authorizeRequisition: null,
+    approveRequisition: null,
+    requisitionToOrder: null,
+    deleteRequisition: null,
+  };
+
   try {
     integrationValidation(inputParameters);
     const { requisition } = inputParameters;
     parameterObject = await createParameterObject(inputParameters);
-    const { requisition: outgoingRequisition } = await createRequisition(parameterObject);
+    const createResult = await createRequisition(parameterObject);
+    httpLog.createRequisition = {
+      request: createResult.request,
+      response: createResult.response,
+    };
+
+    const { requisition: outgoingRequisition } = createResult.response;
+
     parameterObject.requisitionId = outgoingRequisition.id;
     requisitionHasBeenCreated = true;
     parameterObject = {
       ...parameterObject,
       ...(await requisitionMerge(requisition, outgoingRequisition)),
     };
-    await processRequisition(parameterObject);
+    const processLog = await processRequisition(parameterObject);
+    Object.assign(httpLog, processLog);
+
     return {
       requisitionId: parameterObject.requisitionId,
       unmatchedIncomingLines: parameterObject.unmatchedIncomingLines,
       unmatchedOutgoingLines: parameterObject.unmatchedOutgoingLines,
+      httpLog,
     };
   } catch (error) {
     // This is not an errorObject which was deliberately thrown,
     if (!error.code) {
       error = errorObject(ERROR_RUNTIME, error.message);
     }
+    const { request, response, ...rest } = error;
+
+    error.httpLog = {
+      ...error.httpLog,
+      createRequisition: httpLog.createRequisition || {
+        request: request || null,
+        response: { ...response, ...rest } || null,
+      },
+      updateRequisition: httpLog.updateRequisition || {
+        request: request || null,
+        response: { ...response, ...rest } || null,
+      },
+      submitRequisition: httpLog.submitRequisition || {
+        request: request || null,
+        response: { ...response, ...rest } || null,
+      },
+      authorizeRequisition: httpLog.authorizeRequisition || {
+        request: request || null,
+        response: { ...response, ...rest } || null,
+      },
+      approveRequisition: httpLog.approveRequisition || {
+        request: request || null,
+        response: { ...response, ...rest } || null,
+      },
+      requisitionToOrder: httpLog.requisitionToOrder || {
+        request: request || null,
+        response: { ...response, ...rest } || null,
+      },
+    };
 
     if (requisitionHasBeenCreated) {
       try {
-        await deleteRequisition(parameterObject);
+        const deleteResult = await deleteRequisition(parameterObject);
+        httpLog.deleteRequisition = {
+          request: deleteResult.request,
+          response: deleteResult.response,
+        };
         error.wasDeleted = true;
       } catch (deleteError) {
-        error.wasDeleted = false;
-        error.deleteError = deleteError;
+        const { request: deleteRequest, response: deleteResponse, ...deleteRest } = deleteError;
+        error.httpLog.deleteRequisition = {
+          request: deleteRequest || null,
+          response: { ...deleteResponse, ...deleteRest } || null,
+        };
       }
     }
+    delete error.request;
+    delete error.response;
 
     throw error;
   }
